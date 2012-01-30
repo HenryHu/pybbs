f62404bc214b805637452ceac1caa2895adb7ec4
Henry Hu <henry.hu.sh@gmail.com>
2012-01-30 03:13:07 -0500

    fix

--- SemLock.py
+++ SemLock.py
@@ -8,11 +8,11 @@ class SemLock:
     def Init():
         semkey = Config.Config.GetInt("PUBLIC_SEMID", 0x54188)
         try:
-            sems = [Semaphore(semkey)]
+            SemLock.sems = [Semaphore(semkey)]
         except ExistentialError:
-            sems = [Semaphore(semkey, flags = IPC_CREAT | IPC_EXCL, mode = 0700)];
+            SemLock.sems = [Semaphore(semkey, flags = IPC_CREAT | IPC_EXCL, mode = 0700)];
             for i in range(len(sems)):
-                sems[i].release()
+                SemLock.sems[i].release()
 
     @staticmethod
     def Lock(lockid, timeout = None):
