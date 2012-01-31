acee4b4cf8d676fd92414b4251cd2733835a1456
Henry Hu <henry.hu.sh@gmail.com>
2012-01-30 23:10:22 -0500

    modify UserInfo

--- UserInfo.py
+++ UserInfo.py
@@ -13,11 +13,19 @@ class UserInfo:
     def size():
         return UserInfo._parser.size
 
-    def unpack(self, str):
-        Util.Unpack(self, UserInfo._parser.unpack(str))
+    def unpack(self, str = None):
+        if (str):
+            Util.Unpack(self, UserInfo._parser.unpack(str))
+        else:
+            if (self._index < 0):
+                raise Exception("Cannot unpack without index!")
+            else:
+                Util.Unpack(self, UserInfo._parser.unpack(Utmp.utmpshm.read(UserInfo._parser.size, self._index * UserInfo._parser.size)))
 
     def pack(self):
         return UserInfo._parser.pack(*Util.Pack(self))
 
-    def __init__(self, str):
-        self.unpack(str)
+    def __init__(self, idx):
+        self._index = idx - 1
+        if (idx != 0):
+            self.unpack()
