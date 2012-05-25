70c1e58e8381b227c8426cb0e276348517fd4490
Henry Hu <henry.hu.sh@gmail.com>
2012-05-24 23:17:50 -0400

    minor fix

--- errors.py
+++ errors.py
@@ -21,13 +21,13 @@ class error_handler:
 
     def __exit__(self, type, value, tb):
         if (isinstance(value, NoPerm)):
-            svc.return_error(403, value.msg)
+            self.svc.return_error(403, value.msg)
         elif (isinstance(value, WrongArgs)):
-            svc.return_error(400, value.msg)
+            self.svc.return_error(400, value.msg)
         elif (isinstance(value, NotFound)):
-            svc.return_error(404, value.msg)
+            self.svc.return_error(404, value.msg)
         elif (isinstance(value, ServerError)):
-            svc.return_error(500, value.msg)
+            self.svc.return_error(500, value.msg)
         else:
-            svc.return_error(500, traceback.format_exc(value))
+            self.svc.return_error(500, traceback.format_exc(value))
         return True
