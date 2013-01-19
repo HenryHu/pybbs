0ead1326304325f887bb827a96d43d84accd7dab
Henry Hu <henry.hu.sh@gmail.com>
2013-01-18 20:55:49 -0500

    security check

--- resource.py
+++ resource.py
@@ -9,11 +9,24 @@ class Resource:
     @staticmethod
     def GET(svc, session, params, action):
         if Resource.basic_re.match(action):
-            svc.writedata(open(os.path.join(Config.Config.GetString("BBS_DATASVC_ROOT", ""), "res", action)).read())
+            path = os.path.join(Config.Config.GetString("BBS_DATASVC_ROOT", ""), "res", action)
         elif Resource.indir_re.match(action):
             match = Resource.indir_re.match(action)
-            svc.writedata(open(os.path.join(Config.Config.GetString("BBS_DATASVC_ROOT", ""), "res", match.group(1), match.group(2))).read())
+            path = os.path.join(Config.Config.GetString("BBS_DATASVC_ROOT", ""), "res", match.group(1), match.group(2))
         else:
             raise NoPerm("no permission")
+
+        if not os.path.isfile(path):
+            raise NoPerm("no permission")
+
+        path = os.path.realpath(path)
+        if not path.startswith(os.path.join(Config.Config.GetString("BBS_DATASVC_ROOT", ""), "res")):
+            raise NoPerm("no permission")
+
+        svc.writedata(open(path).read())
+
+    @staticmethod
+    def POST(svc, session, params, action):
+        return Resource.GET(svc, session, params, action)
 
 
